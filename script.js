b804e7b310bf5bbbdef445b7e2085c12134c3305
Andie Grant <aegrant08@gmail.com>
2020-10-03 09:43:33 -0400

    add html and css files

---
+++ script.js
@@ -0,0 +1,16 @@
+// create and format landing page in html and css
+// set timer and link to start button
+// score needs to be kept in local storage
+
+// once start button clicked timer starts and page with first question appears (dynamic)
+// add buttons from Bootstrap
+// code one button to correct answer
+// correct answer button increases score and add time
+// code other buttons to wrong answer 
+// incorrect answer buttons deduct points and time
+// next question button goes to next page
+
+// each page will need to be dynamic
+
+
+// at the end initials and score will need to be saved in local storage
